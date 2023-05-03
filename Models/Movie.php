78bd40b82ddfc8fd36c53d39af9cc7eedd806f84
enricoderasmo21 <derasmo.enrico@gmail.com>
2023-05-03 15:27:27 +0200

    Step 1

---
+++ Models/Movie.php
@@ -0,0 +1,26 @@
+<?php
+
+class Movie {
+
+    public $title;
+    public $genre;
+    public $duration;
+    public $nation;
+    public $year;
+    public $vote;
+    public $recommended = false;
+
+    function __construct(string $title, string $genre, int $duration, string $nation, int $year, float $vote) {
+
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->duration = $duration;
+        $this->nation = $nation;
+        $this->year = $year;
+        $this->vote = $vote;
+
+        if($vote >= 2.8){
+            $this->recommended = true;
+        }
+    }
+} 
--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php 
+
+require_once './Models/Movie.php';
+
+$avatar = new Movie("Avatar", "Sci-fi", 162, "USA", 2009, 4.5,);
+$seabiscuit = new Movie("Seabiscuit : Un mito senza tempo", "Biographical", 140, "USA", 2003, 4.2,);
+$ronin = new Movie("47 Ronin", "Azione", 128, "USA", 2013, 2.1,);
+$gli_spietati = new Movie("Gli spietati", "Western", 131, "USA", 1992, 4.2,);
+$will_hunting = new Movie("Will Hunting - Genio ribelle", "Drammatico", 126, "USA", 1992, 4.2,);
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
