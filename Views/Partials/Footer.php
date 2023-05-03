154888f38ac862100086b447f21cd1dacda34ad3
enricoderasmo21 <derasmo.enrico@gmail.com>
2023-05-03 14:40:14 +0200

    created header and footer

---
+++ Views/Partials/Footer.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Blinker:wght@400;600&family=Bungee+Inline&family=Emilys+Candy&family=Fjalla+One&family=Fredoka:wght@300;700&family=Gugi&family=Lato:ital@0;1&family=Libre+Baskerville:ital@1&family=Montserrat:wght@400;600&family=Mynerve&family=Mystery+Quest&family=Noto+Sans:wght@400;700&family=Open+Sans:wght@400;700&family=Roboto:wght@400;500&family=Rowdies:wght@300&family=Rubik+Iso&family=Ubuntu:wght@400;500&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>PhP oop</title>
+</head>
+
+<body>
+    <div id="footer">
+        <div id="social-container">
+            <i class="fa-brands fa-instagram"></i>
+
+            <i class="fa-brands fa-facebook"></i>
+
+            <i class="fa-brands fa-twitter"></i>
+
+            <i class="fa-brands fa-youtube"></i>
+        </div>
+        <div id="copyright">
+            © 1990-2023 by Boolean.com, Inc.
+        </div>
+
+    </div>
+</body>
+</html>
---
+++ Views/Partials/Header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Bebas+Neue&family=Blinker:wght@400;600&family=Bungee+Inline&family=Emilys+Candy&family=Fjalla+One&family=Fredoka:wght@300;700&family=Gugi&family=Lato:ital@0;1&family=Libre+Baskerville:ital@1&family=Montserrat:wght@400;600&family=Mynerve&family=Mystery+Quest&family=Noto+Sans:wght@400;700&family=Open+Sans:wght@400;700&family=Roboto:wght@400;500&family=Rowdies:wght@300&family=Rubik+Iso&family=Ubuntu:wght@400;500&display=swap" rel="stylesheet">
+    
+    
+    <link rel="stylesheet" href="css/style.css">
+    <title>PhP oop</title>
+</head>
+
+<body>
+    <div id="navbar">
+        <div id="logo-container">
+            <img src="img/logo.jpg" alt="">
+        </div>
+        <div id="link-container">
+            <ul id="link-list">
+                <li class="link">Home</li>
+                <li class="link">New</li>
+                <li class="link">Popoular</li>
+                <li class="link">Watchlist</li>
+            </ul>
+        </div>
+    </div> 
+</body>
+</html>
